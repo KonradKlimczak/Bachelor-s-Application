8cdd9779b755699f56913f395e35203c1b7f8924
Konrad Klimczak <konradklimczak93@gmail.com>
2016-05-18 18:36:58 +0200

    Models added

--- application/lesson/models.py
+++ application/lesson/models.py
@@ -1,5 +1,25 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
-# Create your models here.
+class Lesson(models.Model):
+    '''
+    Lesson model
+    '''
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    creator = models.ForeignKey(User)
+    category = models.CharField(max_length=10)
+
+class Question(models.Model):
+    '''
+    Question model
+    '''
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, blank=True)
+    source = models.TextField()
+    answer = models.CharField(max_length=30)
+    category = models.CharField(max_length=10)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
