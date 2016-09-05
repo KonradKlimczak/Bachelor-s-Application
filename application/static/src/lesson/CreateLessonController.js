74c85df662b4c3e7f2ea0331d3b05f203f8681c7
Konrad Klimczak <Konrad Klimczak>
2016-09-05 22:28:33 +0200

    Lesson creator finished v0.5

--- application/static/src/lesson/CreateLessonController.js
+++ application/static/src/lesson/CreateLessonController.js
@@ -5,12 +5,39 @@ angular
     addQuestion();
     $scope.createLesson = createLesson;
     $scope.addQuestion = addQuestion;
+    $scope.addAnswerOption = addAnswerOption;
 
     function createLesson() {
-    
+      $http({
+        method: 'POST',
+        data: {
+          'name': $scope.name,
+          'category': $scope.category,
+          'description': $scope.description,
+          questions: $scope.questions
+        },
+        url: '/api/lesson/create-lesson'
+      }).then(function successCallback(response) {
+        dialogObject = {};
+        dialogObject[response.data.status] = response.data.message;
+        $mdDialog.show(
+          $mdDialog[response.data.status]().locals(dialogObject)
+        );
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        alert('error');
+      });
     }
 
     function addQuestion() {
       $scope.questions.push({});
+    }
+
+    function addAnswerOption(question) {
+      if (!question.answers) {
+        question.answers = [];
+      }
+      question.answers.push('');
     }
   });
